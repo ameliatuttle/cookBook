46e1bc538d2585e9183c43b88313a3b0d9678868
ameliatuttle <aetuttle@byui.edu>
2024-10-29 19:43:32 -0600

    vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        createRecipe: resolve(__dirname, 'src/create-recipe/index.html'),
+        findRecipe: resolve(__dirname, 'src/find-recipe/index.html')
       },
     },
   },
